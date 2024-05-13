dce540d0ba4be42e6d357f6872e3ce6f6926492b
48-iq <ivn25ov@mail.ru>
2024-05-13 11:39:36 +0400

    initial commit

---
+++ backend/IntelliLearn/src/main/java/ru/deadline/destroers/intellilearn/IntelliLearnApplication.java
@@ -0,0 +1,13 @@
+package ru.deadline.destroers.intellilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliLearnApplication.class, args);
+    }
+
+}
---
+++ backend/IntelliLearn/src/test/java/ru/deadline/destroers/intellilearn/IntelliLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.deadline.destroers.intellilearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
