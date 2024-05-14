ebfa982a97b40817ffabd23536336fbe0c140c79
48-iq <ivn25ov@mail.ru>
2024-05-15 02:34:47 +0400

    2:33 user, direction, course - services and controllers

---
+++ backend/IntelliLearn/src/main/java/ru/deadline/destroers/intellilearn/controllers/CoursesController.java
@@ -0,0 +1,2 @@
+package ru.deadline.destroers.intellilearn.controllers;public class CoursesController {
+}
---
+++ backend/IntelliLearn/src/main/java/ru/deadline/destroers/intellilearn/services/impl/CourseServiceImpl.java
@@ -0,0 +1,2 @@
+package ru.deadline.destroers.intellilearn.services.impl;public class CourseServiceImpl {
+}
