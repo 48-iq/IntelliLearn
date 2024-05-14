5345df4bb59114cc0e97d13bc5c1d3343df7636c
48-iq <ivn25ov@mail.ru>
2024-05-14 17:21:35 +0400

    5:21

--- backend/IntelliLearn/src/main/java/ru/deadline/destroers/intellilearn/controllers/NotificationController.java
+++ backend/IntelliLearn/src/main/java/ru/deadline/destroers/intellilearn/controllers/NotificationController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.deadline.destroers.intellilearn.controllers.notification.interfaces.NotificationControllerOperations;
 import ru.deadline.destroers.intellilearn.services.notification.NotificationService;
 
 @RestController
@@ -15,12 +14,12 @@ public class NotificationController {
     private NotificationService notificationService;
 
 
-    public void notifyGroup(String groupId, String message) { // @...Mapping("/...r") public RequestEntity<?> notifyGroup(@ResponseBody SomeDto)
+    public void notifyGroup(Long groupId, String message) { // @...Mapping("/...r") public RequestEntity<?> notifyGroup(@ResponseBody SomeDto)
         notificationService.sendNotificationToGroup(groupId, message);
     }
 
 
-    public void notifyStudent(String studentId, String message) {
+    public void notifyStudent(Long studentId, String message) {
         notificationService.sendNotificationToStudent(studentId, message);
     }
 }
